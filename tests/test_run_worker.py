d37c0c0771c1d1dc06f2a69f84291520138bf440
rachfop <prachford@icloud.com>
2023-11-21 13:56:47 -0800

    updates test

--- tests/test_run_worker.py
+++ tests/test_run_worker.py
@@ -6,7 +6,7 @@ from temporalio.client import WorkflowFailureError
 from temporalio.testing import WorkflowEnvironment
 from temporalio.worker import Worker
 
-from activities import deposit, refund, withdraw
+from activities import BankingActivities
 from shared import PaymentDetails
 from workflows import MoneyTransfer
 
@@ -21,12 +21,12 @@ async def test_money_transfer() -> None:
             amount=250,
             reference_id="12345",
         )
-
+        activities = BankingActivities()
         async with Worker(
             env.client,
             task_queue=task_queue_name,
             workflows=[MoneyTransfer],
-            activities=[withdraw, deposit, refund],
+            activities=[activities.withdraw, activities.deposit, activities.refund],
         ):
             result: str = await env.client.execute_workflow(
                 MoneyTransfer.run,
@@ -48,11 +48,12 @@ async def test_money_transfer_withdraw_insufficient_funds() -> None:
             reference_id="12345",
         )
 
+        activities = BankingActivities()
         async with Worker(
             env.client,
             task_queue=task_queue_name,
             workflows=[MoneyTransfer],
-            activities=[withdraw, deposit, refund],
+            activities=[activities.withdraw, activities.deposit, activities.refund],
         ):
             with pytest.raises(WorkflowFailureError) as excinfo:
                 await env.client.execute_workflow(
@@ -77,11 +78,12 @@ async def test_money_transfer_withdraw_invalid_account() -> None:
             reference_id="12345",
         )
 
+        activities = BankingActivities()
         async with Worker(
             env.client,
             task_queue=task_queue_name,
             workflows=[MoneyTransfer],
-            activities=[withdraw, deposit, refund],
+            activities=[activities.withdraw, activities.deposit, activities.refund],
         ):
             with pytest.raises(WorkflowFailureError) as excinfo:
                 await env.client.execute_workflow(
